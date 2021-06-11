fc1e3cd3f95fd9963d12f60e51b1e9b022fbd5e9
R-Jahongir <rozimov2000@gmail.com>
2021-06-11 12:43:59 +0500

    asd

--- controllers/ShopController.js
+++ controllers/ShopController.js
@@ -41,7 +41,7 @@ exports.getShop = async (req, res) => {
 })
 }
 exports.getContract = async (req, res) => {
-    res.status(200).json({success: true, data: await Shop.find({status: 0}).select({__v: 0})
+    res.status(200).json({success: true, data: await Shop.find({status: 0}).populate('user').select({__v: 0})
 })
 }
 
