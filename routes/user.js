9f1015f7fc623b9b99eaf851ecf6ee86362ed5fa
Zafar6699 <webzafar6699@gmail.com>
2021-06-19 17:05:44 +0500

    asd

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const { protectClient } = require("../middleware/auth");
 router.post("/create", UserController.register);
 router.post("/login", UserController.loginClient);
 router.post("/admin/login", UserController.loginAdmin);
-router.post("/  seller/login", UserController.loginSeller);
+router.post("/seller/login", UserController.loginSeller);
 router.get("/all", UserController.getUsers);
 router.get("/me", protectClient, UserController.me);
 router.delete("/:id", UserController.delete);
