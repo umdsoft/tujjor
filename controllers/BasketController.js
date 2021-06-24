cfa263249238542f7ab7a3f8dfd1e33c93b972be
R-Jahongir <rozimov2000@gmail.com>
2021-06-24 16:19:21 +0500

    asd

--- controllers/BasketController.js
+++ controllers/BasketController.js
@@ -18,27 +18,58 @@ exports.create = (req, res) => {
         });
 };
 exports.getAll = async (req, res) => {
-    await Basket.find({ user: req.params.user })
-        .populate([
-            {
-                path: "param",
-                select: "image -_id",
+    Basket.aggregate([
+        { $match: { user: mongoose.Types.ObjectId(req.params.user) } },
+        {
+            $lookup: {
+                from: "params",
+                let: { param: "$param" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$_id", "$$param"] },
+                        },
+                    },
+                    { $project: { image: 1, _id: 0 } },
+                ],
+                as: "param",
             },
-            {
-                path: "size",
-                select: "size price count -_id",
+        },
+        {
+            $lookup: {
+                from: "sizes",
+                let: { size: "$size" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$_id", "$$size"] },
+                        },
+                    },
+                    { $project: { size: 1, price: 1, count: 1, _id: 0 } },
+                ],
+                as: "param",
             },
-            {
-                path: "product",
-                select: "name image category -_id",
-            },
-        ])
-        .exec((err, data) => {
-            if (err) {
-                return res.status(400).json({ success: false, err });
-            }
-            return res.status(200).json({ success: true, data });
-        });
+        },
+    ]).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({ success: false, err });
+        }
+        return res.status(200).json({ success: true, data });
+    });
+    await Basket.find({ user: req.params.user }).populate([
+        {
+            path: "param",
+            select: "image -_id",
+        },
+        {
+            path: "size",
+            select: "size price count -_id",
+        },
+        {
+            path: "product",
+            select: "name image category -_id",
+        },
+    ]);
 };
 exports.edit = async (req, res) => {
     await Basket.findByIdAndUpdate(
