1b09de422b21ba897e36ff7a113e53e6861c4ad3
R-Jahongir <rozimov2000@gmal.com>
2021-06-29 12:04:07 +0500

    123

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -421,7 +421,7 @@ exports.filter = async (req, res) => {
                         in: "$$brands.brands",
                     },
                 },
-                data: 1,
+                "$data.brand": 0,
             },
         },
     ]).exec(async (err, data) => {
