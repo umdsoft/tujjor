922705c33937ee96b72197f4a01211de66f5a7ec
R-Jahongir <rozimov2000@gmal.com>
2021-06-29 15:41:19 +0500

    123

--- middleware/errorFileUpload.js
+++ middleware/errorFileUpload.js
@@ -1,6 +1,9 @@
 exports.validateFile = (req, res, next) => {
-    if(!(req.file || req.files)) {
-    return res.status(404).json({success: false, message:"File dont Upload"});
+    if (!(req.file || req.files)) {
+        console.log("FILE", req.file, req.files);
+        return res
+            .status(404)
+            .json({ success: false, message: "File dont Upload" });
     }
-    next()
-}
+    next();
+};
