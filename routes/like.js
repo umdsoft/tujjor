64173029f3203d871cdf1c5be57b27be050c67ea
R-Jahongir <rozimov2000@gmal.com>
2021-06-30 23:36:07 +0500

    123

--- routes/like.js
+++ routes/like.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const LikeController = require("../controllers/LikeController");
+const { protectClient } = require("../middleware/auth");
 
-router.post("/create", LikeController.create);
-router.get("/:user", LikeController.getAll);
-router.delete("/:id", LikeController.delete);
+router.post("/create", protectClient, LikeController.create);
+router.get("/:user", protectClient, LikeController.getAll);
+router.delete("/:id", protectClient, LikeController.delete);
 
 module.exports = router;
