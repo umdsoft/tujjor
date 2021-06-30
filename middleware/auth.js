656e16a1c706818b6710259293ea7cb8828bc6c4
R-Jahongir <rozimov2000@gmal.com>
2021-06-30 23:20:06 +0500

    123

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,36 @@
 const JWT = require("jsonwebtoken");
 const User = require("../models/user");
 
-exports.protect = async (role, req, res, next) => {
-    let token;
-    if (req.headers.token && req.headers.token.startsWith("Bearer")) {
-        token = req.headers.token.split(" ")[1];
-    }
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
-    try {
-        //  verify token
-        const decoded = JWT.verify(token, process.env.JWT_SECRET);
-        let user = await User.findById(decoded.id);
-        if ((role === "client" && !!user.role) || role === user.role) {
-            req.user = user._id;
-            next();
-        } else {
+exports.protect = (role) => {
+    return async (req, res, next) => {
+        let token;
+        if (req.headers.token && req.headers.token.startsWith("Bearer")) {
+            token = req.headers.token.split(" ")[1];
+        }
+        if (!token) {
             return res.status(401).json({
                 success: false,
                 data: "No authorize to access this route",
             });
         }
-    } catch (err) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
+        try {
+            //  verify token
+            const decoded = JWT.verify(token, process.env.JWT_SECRET);
+            let user = await User.findById(decoded.id);
+            if ((role === "client" && !!user.role) || role === user.role) {
+                req.user = user._id;
+                next();
+            } else {
+                return res.status(401).json({
+                    success: false,
+                    data: "No authorize to access this route",
+                });
+            }
+        } catch (err) {
+            return res.status(401).json({
+                success: false,
+                data: "No authorize to access this route",
+            });
+        }
+    };
 };
--- routes/like.js
+++ routes/like.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const LikeController = require("../controllers/LikeController");
 const { protect } = require("../middleware/auth");
 
-router.post("/create", protect("client"), LikeController.create);
+// router.post("/create", protect("client"), LikeController.create);
 // router.get("/all", protect("client"), LikeController.getAll);
 // router.delete("/:id", protect("client"), LikeController.delete);
 // router.delete("/all", protectClient, LikeController.deleteAll);
