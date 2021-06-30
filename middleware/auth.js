a0c03acc3790c93ff9dd2d8bc1b51e3e899d0b7b
R-Jahongir <rozimov2000@gmal.com>
2021-06-30 23:16:00 +0500

    123

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,7 @@
 const JWT = require("jsonwebtoken");
 const User = require("../models/user");
-exports.protectClient = async (req, res, next) => {
-    let token;
-    if (req.headers.token && req.headers.token.startsWith("Bearer")) {
-        token = req.headers.token.split(" ")[1];
-    }
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
-    try {
-        //  verify token
-        const decoded = JWT.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
-    } catch (err) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
-};
-exports.protectAdmin = async (req, res, next) => {
+
+exports.protect = role = async (req, res, next) => {
     let token;
     if (req.headers.token && req.headers.token.startsWith("Bearer")) {
         token = req.headers.token.split(" ")[1];
@@ -38,44 +16,15 @@ exports.protectAdmin = async (req, res, next) => {
         //  verify token
         const decoded = JWT.verify(token, process.env.JWT_SECRET);
         let user = await User.findById(decoded.id);
-        if (user.role != "admin") {
+        if ((role === "client" && !!user.role) || role === user.role) {
+            req.user = user._id;
+            next();
+        } else {
             return res.status(401).json({
                 success: false,
                 data: "No authorize to access this route",
             });
         }
-        req.user = user;
-        next();
-    } catch (err) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
-};
-exports.protectSeller = async (req, res, next) => {
-    let token;
-    if (req.headers.token && req.headers.token.startsWith("Bearer")) {
-        token = req.headers.token.split(" ")[1];
-    }
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            data: "No authorize to access this route",
-        });
-    }
-    try {
-        //  verify token
-        const decoded = JWT.verify(token, process.env.JWT_SECRET);
-        let user = await User.findById(decoded.id);
-        if (user.role != "seller") {
-            return res.status(401).json({
-                success: false,
-                data: "No authorize to access this route",
-            });
-        }
-        req.user = user;
-        next();
     } catch (err) {
         return res.status(401).json({
             success: false,
