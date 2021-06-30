cc46d07a292ee039b0574e4c30ff376fbf861a3e
R-Jahongir <rozimov2000@gmal.com>
2021-06-30 17:40:47 +0500

    asd

--- routes/regions.js
+++ routes/regions.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const Regions = require('../controllers/Regions')
-router.post('/create',Regions.addRegion)
-router.get('/all',Regions.getRegions)
-router.put('/:id',Regions.editRegion)
-router.delete('/:id',Regions.deleteRegion)
+const express = require("express");
+const router = express.Router();
+const Regions = require("../controllers/RegionsController");
+router.post("/create", Regions.addRegion);
+router.get("/all", Regions.getRegions);
+router.put("/:id", Regions.editRegion);
+router.delete("/:id", Regions.deleteRegion);
 
-router.post('/district/create',Regions.addDistrict)
-router.put('/district/:id',Regions.updateDistrict)
-router.delete('/district/:id',Regions.deleteDistrict)
+router.post("/district/create", Regions.addDistrict);
+router.put("/district/:id", Regions.updateDistrict);
+router.delete("/district/:id", Regions.deleteDistrict);
 
 module.exports = router;
