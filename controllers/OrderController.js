0ea1dbdafdf9e2094db01671a94388c811c0ff4c
R-Jahongir <rozimov2000@gmal.com>
2021-07-03 17:19:26 +0500

    asda

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -2,7 +2,7 @@ const Order = require("../models/order");
 const Shop = require("../models/shop");
 
 exports.create = async (req, res) => {
-    const count = Order.countDocuments({}) + 1;
+    const count = Order.countDocuments({});
     console.log(count);
     const product = [];
     req.body.product &&
