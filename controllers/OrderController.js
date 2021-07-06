79b82a179d9eda80ceb63068d352e042f10ddd2a
R-Jahongir <rozimov2000@gmal.com>
2021-07-06 16:12:09 +0500

    asjdh

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -2,32 +2,32 @@ const Order = require("../models/order");
 const Shop = require("../models/shop");
 
 exports.create = (req, res) => {
-    Order.countDocuments({}, (err, count) => {
-        let product =
-            req.body.products &&
-            req.body.products.map((element) => {
-                console.log(element);
-                Shop.findById({ _id: element.shop }).then((shop) => {
-                    return {
-                        ...element,
-                        account: shop.shopId,
-                    };
-                });
-            });
-        console.log(product);
+    Order.countDocuments({}, async (err, count) => {
+        const product = [];
+
         const order = new Order({
             user: req.user,
             amount: req.body.amount,
             orderId: count,
             address: {
-                region: req.body.address ? req.body.address.region : null,
-                district: req.body.address ? req.body.address.district : null,
-                address: req.body.address ? req.body.address.address : null,
-                phone: req.body.address ? req.body.address.phone : null,
+                region: req.body.address ? req.body.address.region : "",
+                district: req.body.address ? req.body.address.district : "",
+                address: req.body.address ? req.body.address.address : "",
             },
-            products: [...product],
+            products: req.body.product
+                ? req.body.product.forEach(async (element) => {
+                      await Shop.findById({ _id: element.shop }).then(
+                          (shop) => {
+                              product.push({
+                                  ...element,
+                                  account: shop.shopId,
+                              });
+                          }
+                      );
+                  })
+                : [],
         });
-        order
+        await order
             .save()
             .then(() => {
                 res.status(201).json({ success: true, data: order });
