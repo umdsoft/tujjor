a7e45da500f012a517ea352db83357010bdf02e5
R-Jahongir <rozimov2000@gmal.com>
2021-07-10 23:56:12 +0500

    123

--- controllers/Statistic.js
+++ controllers/Statistic.js
@@ -1,25 +1,26 @@
 const PayedList = require("../models/payedList");
 
 exports.statShop = async (req, res) => {
-    PayedList.aggregate([
-        {
-            $match: {
-                createdAt: {
-                    $and: [
-                        { createdAt: { $gte: new Date(req.body.start) } },
-                        { createdAt: { $lte: new Date(req.body.end) } },
-                    ],
-                },
-            },
-        },
-        // {
-        //     $group: {
-        //         _id: "$shop",
-        //         amount: { $sum: "$amount" },
-        //         count: { $sum: "$count" },
-        //     },
-        // },
-    ]).exec((err, data) => {
+    // PayedList.aggregate([
+    // {
+    //     $match: {
+    //         createdAt: {
+    //             $and: [
+    //                 { createdAt: { $gte: new Date(req.body.start) } },
+    //                 { createdAt: { $lte: new Date(req.body.end) } },
+    //             ],
+    //         },
+    //     },
+    // },
+    // {
+    //     $group: {
+    //         _id: "$shop",
+    //         amount: { $sum: "$amount" },
+    //         count: { $sum: "$count" },
+    //     },
+    // },
+    // ])
+    PayedList.find({}).exec((err, data) => {
         if (err) return res.status(400).json({ success: false, err });
         res.status(200).json({
             success: true,
