8ee6c286e7c4d3a4f2a8f509582f5bccb632143b
R-Jahongir <rozimov2000@gmal.com>
2021-07-24 10:08:20 +0500

    123

--- routes/tag.js
+++ routes/tag.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const TagController = require("../controllers/TagController");
 
 router.post("/create", TagController.create);
-router.get("/all", TagController.getAll);
+router.get("/all", TagController.all);
 router.delete("/:id", TagController.delete);
 
 module.exports = router;
