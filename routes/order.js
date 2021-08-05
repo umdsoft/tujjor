ea79a5c2a4f3cf243d862e8d401b23c464066b5e
R-Jahongir <rozimov2000@gmal.com>
2021-08-05 15:35:10 +0500

    123

--- routes/order.js
+++ routes/order.js
@@ -6,5 +6,6 @@ router.post("/create", protectClient, OrderController.create);
 router.get("/all", OrderController.getAll);
 router.get("/me", protectClient, OrderController.getMeOrder);
 router.put("/:id", OrderController.update);
+router.get("/one/:id", OrderController.getById);
 
 module.exports = router;
