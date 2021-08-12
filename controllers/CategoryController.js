d3fa04d79f162349e251a0f4b927ab81f099253f
R-Jahongir <rozimov2000@gmal.com>
2021-08-12 17:59:07 +0500

    123

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -23,27 +23,23 @@ function createCategories(categories, parentId = null) {
     }
     return categoryList;
 }
-function getCategoriesCreate(parentId) {
-    
-    return Promise.all(
-        Category.find({parentId: parentId}).then(category=>{
-            let categories = []
-            if(category.length){
-                category.forEach(async key=>{
-                    categories.push({
-                        _id: key._id,
-                        name: key.name,
-                        parentId: key.parentId,
-                        slug: key.slug,
-                        children: await getCategoriesCreate(key._id),
-                    });
-                })
-            }
-            console.log(categories)
-            return categories;
-        })
-    ) 
-    
+function getCategoriesCreate(categories, parentId) {
+    const categoryList = [];
+    let category;
+    category = categories.filter((cat) => cat.parentId == parentId);
+    for (let cate of category) {
+        categoryList.push({
+            _id: cate._id,
+            name: {
+                uz: cate.name.uz,
+                ru: cate.name.ru,
+            },
+            parentId: cate.parentId,
+            slug: cate.slug,
+            children: createCategories(categories, cate._id),
+        });
+    }
+    return categoryList;
 }
 const deleteCategory = async (parentId) => {
     let item = await Category.find({ parentId });
@@ -91,12 +87,12 @@ exports.getOne = async (req, res) => {
     if (!req.params.id) {
         return res.status(400).json({ success: false, message: "Required" });
     }
-    await Category.findOne({ _id: req.params.id}).exec((err, category) => {
+    await Category.find().exec((err, categories) => {
         if (err) {return res.status(400).json({ err });}
-        if (category) {
-            console.log(category);
-            const categoryList = getCategoriesCreate(category._id)
-            res.status(200).json({success: true, data:categoryList})
+        console.log(categories)
+        if (categories) {
+            const categoryList = getCategoriesCreate(categories, req.params.id);
+            res.status(200).json({ success: true, data: categoryList });
         }
     });
 };
