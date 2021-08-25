2633772e1ea27bb715f6d5e5759d680f8f3d260d
R-Jahongir <rozimov2000@gmal.com>
2021-08-25 16:37:27 +0500

    123

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const OrderController = require("../controllers/OrderController");
-const { protectClient } = require("../middleware/auth");
+const { protectClient, protectSeller } = require("../middleware/auth");
 
 router.post("/create", protectClient, OrderController.create);
-router.get("/all", OrderController.getAll);
+router.get("/all",protectSeller, OrderController.getAll);
 router.get("/me", protectClient, OrderController.getMeOrder);
 router.put("/:id", OrderController.update);
 router.get("/one/:id", OrderController.getById);
