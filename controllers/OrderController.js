92c3b56aad7e6f176c91415fe6413bb8809b7ed8
R-Jahongir <rozimov2000@gmal.com>
2021-08-26 12:37:12 +0500

    123

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -99,9 +99,9 @@ exports.create = (req, res) => {
 
 exports.getById = async (req, res) => {
     const shop = await Shop.findOne({user: req.user})
-    const order = await Order.findOne({_id: req.params.id, payed: 1},{amount: 1, orderId: 1, address: 1})
+    const order = await Order.findOne({orderId: req.params.orderId, payed: 1},{amount: 1, orderId: 1, address: 1})
     await OrderProducts.aggregate([
-        {$match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1, orderId: order.orderId} },
+        {$match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1, orderId: req.params.orderId} },
         {$project: {
             name: 1,
             image: 1,
