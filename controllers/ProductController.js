ebb4ad4d45a2faf6cb6b5c87c985ecd48aaf57dc
R-Jahongir <rozimov2000@gmal.com>
2021-09-08 19:21:25 +0500

    123

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -26,6 +26,7 @@ async function createSizeDiscount(index, data, body){
     obj["discount"] = (obj.price * (100 - body.discount)) / 100;
     obj["discount_start"] = new Date(body.start);
     obj["discount_end"] = new Date(body.end);
+    console.log("SIZE ", index)
     await obj.save()
     if (index === data.length - 1) {
         return ;
@@ -37,6 +38,7 @@ async function updateProductMinSize(index, data){
     const size = await Size.find({productId: data[index]}, 
         {price: 1, discount: 1, discount_percent: 1, discount_start: 1, discount_end: 1, _id: 0}
     ).sort({price: 1}).limit(1)
+    console.log("PRODUCT ", index)
     await Product.findByIdAndUpdate({ _id: data[index] },{ $set: {minSize: size[0]}})
     if(index === data.length - 1){
         return
