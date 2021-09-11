21ece4634dad2c7547309b3be183cb0a09f06e7a
R-Jahongir <rozimov2000@gmal.com>
2021-09-11 22:31:01 +0500

    321

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -334,8 +334,12 @@ exports.editParam = async (req, res) => {
 exports.editSize = async (req, res) => {
     await Size.findByIdAndUpdate({ _id: req.params.id }, { $set: {...req.body, discount: null, discount_percent: null, discount_start: null, discount_end: null}}, {new: true}).exec((err, data) => {
         if (err) return res.status(400).json({ success: false, data: err });
-        Product.findOneAndUpdate({ productId: data.productId },{ $set: { minSize: {price: data.price, discount: null, discount_percent: null, discount_start: null, discount_end: null }} })
-        return res.status(200).json({ success: true});
+        console.log("DATA ", data);
+        Product.findOneAndUpdate({ productId: data.productId },{ $set: { minSize: {price: data.price, discount: null, discount_percent: null, discount_start: null, discount_end: null }} }).then(()=>{
+            return res.status(200).json({ success: true});
+        }).catch(err => {
+            res.status(400).json({ success: false, err})
+        })
     });
 };
 
