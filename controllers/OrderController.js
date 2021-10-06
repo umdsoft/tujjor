6a1ec587497acd87094c27c8ca82274fc892c8e9
R-Jahongir <rozimov2000@gmail.com>
2021-10-06 20:07:12 +0500

    123

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -6,197 +6,249 @@ const Product = require("../models/product");
 const Param = require("../models/param");
 const mongoose = require("mongoose");
 exports.create = (req, res) => {
-    if(!(req.body.products && req.body.products.length)){
-        return res
-            .status(400)
-            .json({ success: false, message: "Something went wrong" });
-    }
-    Order.countDocuments({}, async (err, count) => {
-        let summ = 0;
-        const DOSTAVKA_PRICE = 20000;
-        let shops =  [];
-        const products = await Promise.all(
-            req.body.products.map(async (element) => {
-                let size = await Size.findById({_id: element.size});
-                let product = await Product.findById({_id: element.product});
-                let param = await Param.findById({_id: element.param});
-                let shop = await Shop.findById({ _id: product.shop });
-                if(shops.indexOf(shop._id.toString()) === -1) shops.push(shop._id.toString());
-                if(size.discount && new Date(size.discount_start) <= new Date() && new Date(size.discount_end) >= new Date()){
-                if (size.discount !== element.amount) {
-                    return;
-                } else {
-                    summ += size.discount * element.count
-                }               
-                } else {
-                    if (size.price !== element.amount) {
+    try {
+        
+        if(!(req.body.products && req.body.products.length)){
+            return res
+                .status(400)
+                .json({ success: false, message: "Something went wrong" });
+        }
+        Order.countDocuments({}, async (err, count) => {
+            let summ = 0;
+            const DOSTAVKA_PRICE = 20000;
+            let shops =  [];
+            const products = await Promise.all(
+                req.body.products.map(async (element) => {
+                    let size = await Size.findById({_id: element.size});
+                    let product = await Product.findById({_id: element.product});
+                    let param = await Param.findById({_id: element.param});
+                    let shop = await Shop.findById({ _id: product.shop });
+                    if(shops.indexOf(shop._id.toString()) === -1) shops.push(shop._id.toString());
+                    if(size.discount && new Date(size.discount_start) <= new Date() && new Date(size.discount_end) >= new Date()){
+                    if (size.discount !== element.amount) {
                         return;
                     } else {
-                        summ += size.price*element.count
-                    }
-                };
-                return {
-                    status: 0,
-                    orderId: count,
-                    user: req.user,
-                    count: element.count,
-                    //product Items
-                    productId: product._id,
-                    name: product.name,
-                    image: product.image,
-                    description: product.description,
-                    category: product.category,
-                    brand: product.brand,
-                    //param Items
-                    paramId: param._id,
-                    paramImage: param.image,
-                    //size Items
-                    sizeId: size._id,
-                    size: size.size,
-                    amount: element.amount,
-                    //shop Items
-                    shopId: shop._id,
-                    account: shop.shopId,
-                    percent: shop.percent
-                };
-            })
-        )
-        const dostavka = req.body.toMyHouse ? shops.length * DOSTAVKA_PRICE: 0
-        summ += dostavka;
-        const order = new Order({
-            user: req.user,
-            amount: req.body.amount,
-            orderId: count,
-            address: {
-                region: req.body.address ? req.body.address.region : null,
-                district: req.body.address ? req.body.address.district : null,
-                address: req.body.address ? req.body.address.address : null,
-                phone: req.body.address ? req.body.address.phone : null,
-            },
-            shopCount : shops.length,
-            dostavka : dostavka
-        });
-        if (summ !== req.body.amount || !products.length) {
-            console.log(summ, req.body.amount, shops.length, shops)
-            return res.status(400).json({ success: false, message: "Something went wrong" });
-        }
-        products.forEach((element, index)=>{
-            if(index === products.length-1){
-                new OrderProducts(element).save().then(()=>{
-                order.save().then(() => {
-                        return res.status(201).json({ success: true, data: {orderId: order.orderId, amount: order.amount}});
-                    })
-                    .catch((err) => {
-                       return res.status(400).json({ success: false, err });
-                    });
-                }).catch(err=>{
-                    OrderProducts.deleteMany({orderId: order.orderId})
-                    return res.status(400).json({ success: false, err });
+                        summ += size.discount * element.count
+                    }               
+                    } else {
+                        if (size.price !== element.amount) {
+                            return;
+                        } else {
+                            summ += size.price*element.count
+                        }
+                    };
+                    return {
+                        status: 0,
+                        orderId: count,
+                        user: req.user,
+                        count: element.count,
+                        //product Items
+                        productId: product._id,
+                        name: product.name,
+                        image: product.image,
+                        description: product.description,
+                        category: product.category,
+                        brand: product.brand,
+                        //param Items
+                        paramId: param._id,
+                        paramImage: param.image,
+                        //size Items
+                        sizeId: size._id,
+                        size: size.size,
+                        amount: element.amount,
+                        //shop Items
+                        shopId: shop._id,
+                        account: shop.shopId,
+                        percent: shop.percent
+                    };
                 })
-            } else {
-                new OrderProducts(element).save().catch(err=>{
-                    OrderProducts.deleteMany({orderId: order.orderId})
-                    return res.status(400).json({ success: false, err });
-                })
+            )
+            const dostavka = req.body.toMyHouse ? shops.length * DOSTAVKA_PRICE: 0
+            summ += dostavka;
+            const order = new Order({
+                user: req.user,
+                amount: req.body.amount,
+                orderId: count,
+                address: {
+                    region: req.body.address ? req.body.address.region : null,
+                    district: req.body.address ? req.body.address.district : null,
+                    address: req.body.address ? req.body.address.address : null,
+                    phone: req.body.address ? req.body.address.phone : null,
+                },
+                shopCount : shops.length,
+                dostavka : dostavka
+            });
+            if (summ !== req.body.amount || !products.length) {
+                return res.status(400).json({ success: false, message: "Something went wrong" });
             }
-        })
-        
-        
-    });
+            products.forEach((element, index)=>{
+                if(index === products.length-1){
+                    new OrderProducts(element).save().then(()=>{
+                    order.save().then(() => {
+                            return res.status(201).json({ success: true, data: {orderId: order.orderId, amount: order.amount}});
+                        })
+                        .catch((err) => {
+                           return res.status(400).json({ success: false, err });
+                        });
+                    }).catch(err=>{
+                        OrderProducts.deleteMany({orderId: order.orderId})
+                        return res.status(400).json({ success: false, err });
+                    })
+                } else {
+                    new OrderProducts(element).save().catch(err=>{
+                        OrderProducts.deleteMany({orderId: order.orderId})
+                        return res.status(400).json({ success: false, err });
+                    })
+                }
+            })
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Something went wrong"});
+    }
 };
 exports.getById = async (req, res) => {
-    const shop = await Shop.findOne({user: req.user})
-    const order = await Order.findOne({
-        orderId: req.params.orderId,
-        payed: 1
-    },{amount: 1, orderId: 1, address: 1})
-    await OrderProducts.aggregate([
-        {$match: { shopId: mongoose.Types.ObjectId(shop._id), payed: 1, orderId: parseInt(req.params.orderId)} },
-        {$project: {
-            name: 1,
-            image: 1,
-            paramImage: 1,
-            size: 1,
-            amount: 1,
-            count: 1,
-            description: 1,
-            status: 1,
-            orderId: 1
-        }}
-    ]).exec((err, data) => {
-        if (err) return res.status(400).json({ success: false, err });
-        res.status(200).json({
-            success: true,
-            data: {
-                amount: order.amount,
-                orderId: order.orderId,
-                address: order.address,
-                products: data
-            }
+    try {
+        
+        const shop = await Shop.findOne({user: req.user})
+        const order = await Order.findOne({
+            orderId: req.params.orderId,
+            payed: 1
+        },{amount: 1, orderId: 1, address: 1})
+        await OrderProducts.aggregate([
+            {$match: { shopId: mongoose.Types.ObjectId(shop._id), payed: 1, orderId: parseInt(req.params.orderId)} },
+            {$project: {
+                name: 1,
+                image: 1,
+                paramImage: 1,
+                size: 1,
+                amount: 1,
+                count: 1,
+                description: 1,
+                status: 1,
+                orderId: 1
+            }}
+        ]).exec((err, data) => {
+            if (err) return res.status(400).json({ success: false, err });
+            res.status(200).json({
+                success: true,
+                data: {
+                    amount: order.amount,
+                    orderId: order.orderId,
+                    address: order.address,
+                    products: data
+                }
+            });
         });
-    });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Something went wrong"})
+    }
 }
 exports.getAll = async (req, res) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-    const status = parseInt(req.query.status);
-    const shop = await Shop.findOne({user: req.user})
-    let count = 0;
-    await OrderProducts.aggregate([ 
-        { $match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1} },
-        {
-            $group: {_id: "$orderId"}
-        },
-        {
-            $group: {_id: "$_id", count: {$sum: 1}}
-        },
-    ]).exec((err, data)=>{
-        if(err) return res.status(400).json({ success: false, err })
-        count = data[0]?data[0].count:0
-    })
-    await OrderProducts.aggregate([
-        { $match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1} },
-        {$project: {
-            name: 1,
-            image: 1,
-            paramImage: 1,
-            size: 1,
-            amount: 1,
-            count: 1,
-            description: 1,
-            status: 1,
-            orderId: 1
-        }},
-        {$group: {
-            _id: "$orderId",
-            products: {$push: "$$ROOT"}
-        }},
-        { $skip: (page - 1) * limit }, 
-        { $limit: limit },
-        {$lookup: {
-            from: "orders",
-            localField: "_id",
-            foreignField: "orderId",
-            as: "order"
-        }},
-        {$unwind: "$order"},
-        {$project: {
-            products: 1,
-            amount: "$order.amount",
-            orderId: "$order.orderId",
-            address: "$order.address",
-            createdAt: "$order.createdAt",
-            _id: 0
-        }},
-        {$sort: {createdAt: -1}},
-    ]).exec((err, data) => {
-        if (err) return res.status(400).json({ success: false, err });
-        res.status(200).json({
-            success: true,
-            data,
-            count
+    try {    
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const status = parseInt(req.query.status);
+        const shop = await Shop.findOne({user: req.user})
+        let count = 0;
+        await OrderProducts.aggregate([ 
+            { $match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1} },
+            {
+                $group: {_id: "$orderId"}
+            },
+            {
+                $group: {_id: "$_id", count: {$sum: 1}}
+            },
+        ]).exec((err, data)=>{
+            if(err) return res.status(400).json({ success: false, err })
+            count = data[0]?data[0].count:0
+        })
+        await OrderProducts.aggregate([
+            { $match: { status: status, shopId: mongoose.Types.ObjectId(shop._id), payed: 1} },
+            {$project: {
+                name: 1,
+                image: 1,
+                paramImage: 1,
+                size: 1,
+                amount: 1,
+                count: 1,
+                description: 1,
+                status: 1,
+                orderId: 1
+            }},
+            {$group: {
+                _id: "$orderId",
+                products: {$push: "$$ROOT"}
+            }},
+            { $skip: (page - 1) * limit }, 
+            { $limit: limit },
+            {
+                $lookup: {
+                    from: "orders",
+                    let: { orderId: "$_id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ["$paramId", "$$paramId"],
+                                },
+                            },
+                        },
+                        {$lookup: {
+                            from: "regions",
+                            localField: "address.region",
+                            foreignField: "_id",
+                            as: "address.region"
+                        }},
+                        {$lookup: {
+                            from: "districts",
+                            localField: "address.district",
+                            foreignField: "_id",
+                            as: "address.district"
+                        }},
+                        {$lookup: {
+                            from: "users",
+                            localField: "user",
+                            foreignField: "_id",
+                            as: "user"
+                        }},
+                        {
+                            $project: {
+                                user: {
+                                    name: 1
+                                },
+                                amount: 1,
+                                orderId: 1,
+                                address: 1,
+                                createdAt: 1
+                            }
+                        }
+                    ],
+                    as: "order"
+                }
+            },
+            {$unwind: "$order"},
+            {$project: {
+                products: 1,
+                user: "$order.user",
+                amount: "$order.amount",
+                orderId: "$order.orderId",
+                address: "$order.address",
+                createdAt: "$order.createdAt",
+                _id: 0
+            }},
+
+            {$sort: {createdAt: -1}},
+        ]).exec((err, data) => {
+            if (err) return res.status(400).json({ success: false, err });
+            res.status(200).json({
+                success: true,
+                data,
+                count
+            });
         });
-    });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Something went wrong"});
+    }
 };
 exports.update = async (req, res) => {
     try {
@@ -212,7 +264,7 @@ exports.update = async (req, res) => {
             res.status(400).json({success: false})
         })
     } catch (error) {
-        console.log("ERROR ", error)
+        tes.status(500).json({success: false, message: "Something went wrong"});
     }
 }
 exports.delivered = async (req, res) => {
