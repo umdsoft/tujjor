3d5ca9703da9f3b0d32a96f83d7cda5f9c521393
R-Jahongir <72991667+R-Jahongir@users.noreply.github.com>
2021-10-08 11:01:21 +0500

    Update applicationShop.js

--- routes/applicationShop.js
+++ routes/applicationShop.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const ApplicationShopController = require('../controllers/ApplicationShopController')
+const { protectUser, protectSeller } = require("../middleware/auth");
 
-router.post('/create', ApplicationShopController.create);
+router.post('/create', protectUser, ApplicationShopController.create);
 router.get('/all', ApplicationShopController.getAll);
-router.get('/:user', ApplicationShopController.getOne);
+router.get('/me', protectUser, ApplicationShopController.getOne);
 router.put('/status/:id', ApplicationShopController.editStatus);
 router.delete('/:id', ApplicationShopController.delete);
 
