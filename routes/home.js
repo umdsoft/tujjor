e005bfcfd6dc36fe8b9fbd066cc668581a606153
R-Jahongir <rozimov2000@gmail.com>
2021-10-10 23:06:04 +0500

    123

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,5 @@ const Controller = require("../controllers/Controller");
 
 router.get("/discount", Controller.getDiscounts);
 router.get("/popular", Controller.popularProducts);
-router.get("/all", Controller.all);
 
 module.exports = router;
