4b1029090142867c3c0f60acb3b908839677029c
Umidbek Jumaniyozov <ujumaniyozov@ya.ru>
2021-11-13 19:12:03 +0500

    ASD

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/tujjor";
+const url = "mongodb://127.0.0.1:27017/tujjor";
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(url, {
