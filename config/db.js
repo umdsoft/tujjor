6bd7503d9112db6172c7349ffab70e1f87e767c4
Umidbek Jumaniyozov <ujumaniyozov@ya.ru>
2021-11-13 19:05:19 +0500

    ASD

--- config/db.js
+++ config/db.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/tujjor";
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(url, {
-            useNewUrlParser: true
-        });
-        console.log(`MongoDB connected : ${conn.connection.host}`);
-    } catch (err) {
-        console.log(err)
-    }
+	try {
+		const conn = await mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log(`MongoDB connected : ${conn.connection.host}`);
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 module.exports = connectDB;
