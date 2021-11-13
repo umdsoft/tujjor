d581de72ae5a764bebf7f70cd329bd5cd60805b0
umdsoft <63510583+umdsoft@users.noreply.github.com>
2021-11-13 18:41:33 +0500

    Update db.js
    sds

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ const url = "mongodb://localhost:27017/tujjor";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
+            useNewUrlParser: true
         });
         console.log(`MongoDB connected : ${conn.connection.host}`);
     } catch (err) {
