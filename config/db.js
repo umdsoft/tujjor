18ff400dba22d81b06f18cc56ca6f0bc308aae58
R-Jahongir <rozimov2000@gmail.com>
2021-11-13 18:36:16 +0500

    123

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const url = "mongodb://localhost:27017/tujjor";
 const connectDB = async () => {
     try {
-        setTimeout(function() {
+        setTimeout(async function() {
             const conn = await mongoose.connect(url, {
                 useNewUrlParser: true,
                 useCreateIndex: true,
