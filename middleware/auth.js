7ad2b8f51040ef635fce2ae9b4741949d0579319
R-Jahongir <rozimov2000@gmail.com>
2021-12-24 10:57:34 +0500

    new auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,37 @@
 const JWT = require("jsonwebtoken");
 const User = require("../models/user");
 const Client = require("../models/client");
+exports.protectUser = async (req, res, next) => {
+    let token;
+    if (req.headers.token && req.headers.token.startsWith("Bearer")) {
+        token = req.headers.token.split(" ")[1];
+    }
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            data: "No authorize to access this route",
+        });
+    }
+    try {
+        //  verify token
+        const decoded = JWT.verify(token, process.env.JWT_SECRET);
+        let user = await User.findById(decoded.id);
+        if (!!user) {
+            req.user = user._id;
+            next();
+        } else {
+            return res.status(401).json({
+                success: false,
+                data: "No authorize to access this route",
+            });
+        }
+    } catch (err) {
+        return res.status(401).json({
+            success: false,
+            data: "No authorize to access this route",
+        });
+    }
+};
 exports.protectClient = async (req, res, next) => {
     let token;
     if (req.headers.token && req.headers.token.startsWith("Bearer")) {
