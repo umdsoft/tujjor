a949600be6ecc4f3a5bcdb041b18bb2391f0d192
zafarich <zqalandarov@texnomart.uz>
2024-04-02 13:48:25 +0500

    asd

--- config/db.js
+++ config/db.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://127.0.0.1:27017/tujjor";
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		});
-		console.log(`MongoDB connected : ${conn.connection.host}`);
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const conn = await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB connected : ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -8,44 +8,48 @@ const connect = require("./config/db");
 const path = require("path");
 const fs = require("fs");
 const redis = require("redis");
-const { promisify } = require('util')
-const {shouldCompress} = require("./utils")
-const compression = require('compression');
-const Routes = require('./routes')
+const {promisify} = require("util");
+const {shouldCompress} = require("./utils");
+const compression = require("compression");
+const Routes = require("./routes");
 //Connect MongoDB
 connect();
 
 // Settings
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({path: "./config/config.env"});
 app.use(express.static(path.join(__dirname, "public")));
-app.use(logger("dev", {
-    'skip': (req, res) => req.method === "OPTIONS"
-}));
-app.use(compression({
-  filter: shouldCompress,
-  threshold: 0
-}));
+app.use(
+  logger("dev", {
+    skip: (req, res) => req.method === "OPTIONS",
+  })
+);
+app.use(
+  compression({
+    filter: shouldCompress,
+    threshold: 0,
+  })
+);
 app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("Success working Server");
+  res.send("Success working Server");
 });
-const client = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379,
-})
-app.use((req, res, next) => {
-    try {
-        req.GET_ASYNC = promisify(client.get).bind(client)
-        req.SET_ASYNC = promisify(client.set).bind(client)
-        next();
-    } catch (error) {
-        return res.status(500).json({message: error.message});
-    }
-}, Routes)
+// const client = redis.createClient({
+//   host: "127.0.0.1",
+//   port: 6379,
+// });
+// app.use((req, res, next) => {
+//   try {
+//     req.GET_ASYNC = promisify(client.get).bind(client);
+//     req.SET_ASYNC = promisify(client.set).bind(client);
+//     next();
+//   } catch (error) {
+//     return res.status(500).json({message: error.message});
+//   }
+// }, Routes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log("Server running");
+  console.log("Server running");
 });
