32dcd9e67fd07e8e67a06fed02671ddceb613e3c
zafarich <zqalandarov@texnomart.uz>
2024-05-24 18:28:00 +0500

    asd

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const fs = require("fs");
 const {promisify} = require("util");
 const {shouldCompress} = require("./utils");
 const compression = require("compression");
+const errorHandler = require("./middleware/error");
 const router = require("./routes");
 //Connect MongoDB
 connect();
@@ -36,7 +37,29 @@ app.get("/", (req, res) => {
   res.send("Success working Server");
 });
 
-app.use("/", router);
+router.use("/api/home", require("./routes/home"));
+router.use("/api/stat", require("./routes/statistic"));
+router.use("/api/message", require("./routes/message"));
+router.use("/api/payme", require("./routes/payment"));
+router.use("/api/region", require("./routes/regions"));
+router.use("/api/like", require("./routes/like"));
+router.use("/api/order", require("./routes/order"));
+router.use("/api/tag", require("./routes/tag"));
+router.use("/api/basket", require("./routes/basket"));
+router.use("/api/category", require("./routes/category"));
+router.use("/api/banner", require("./routes/banner"));
+router.use("/api/slider", require("./routes/slider"));
+router.use("/api/question", require("./routes/question"));
+router.use("/api/application", require("./routes/applicationShop"));
+router.use("/api/user", require("./routes/user"));
+router.use("/api/shop", require("./routes/shop"));
+router.use("/api/brand", require("./routes/brand"));
+router.use("/api/uploads", require("./routes/images"));
+router.use("/api/help", require("./routes/help"));
+router.use("/api/info", require("./routes/info"));
+router.use("/api/news", require("./routes/news"));
+router.use("/api/product", require("./routes/product"));
+router.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server running", PORT);
